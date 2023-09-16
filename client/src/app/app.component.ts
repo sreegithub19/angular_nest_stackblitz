1ef1baecbc572c9a0b95f71265fafc2143ab2a23
sreegithub19 <55496113+sreegithub19@users.noreply.github.com>
2023-09-16 23:51:17 +0530

    4 files modified

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,47 @@ import { ApiService } from 'src/.api-client/services/api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <mat-toolbar color="primary">
+  <span>angular-nest</span>
+</mat-toolbar>
+
+<div class="content">
+  <div class="cards">
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>What is this?</mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        Simple web app template with Angular v15 + NestJS + ng-openapi-gen.<br />
+        <a
+          href="https://github.com/mugifly/angular-nest"
+          target="_blank"
+          rel="noopener"
+        >
+          https://github.com/mugifly/angular-nest
+        </a>
+        <hr />
+        Access some of the NestJS applications
+        <a href="/api/applications" rel="noopener"> here </a>
+      </mat-card-content>
+    </mat-card>
+
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Example request to NestJS</mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        {{ $getHello | async }}
+      </mat-card-content>
+      <mat-card-actions align="end">
+        <button (click)="getHello()" mat-button>Request</button>
+      </mat-card-actions>
+    </mat-card>
+  </div>
+</div>
+
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
